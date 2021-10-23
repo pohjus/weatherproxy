1b61e2737e1955594b84c1fc078e437c18dc1aa1
Jussi Pohjolainen <jussi.pohjolainen@gmail.com>
2021-10-23 18:36:14 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const app = express();
+const axios = require("axios");
+const port = process.env.PORT || 8080;
+
+require("dotenv").config();
+
+app.use(express.static("public"));
+
+app.get("/q=:city([a-z]*)", async (req, res) => {
+  const API_KEY = process.env.API_KEY;
+  const cityName = req.params.city;
+  const url = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${API_KEY}`;
+  let response = await axios.get(url);
+  res.send(response.data);
+});
+
+const server = app.listen(port, () => {
+  console.log(`Listening on port ${port}`);
+});
